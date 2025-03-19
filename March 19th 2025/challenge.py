b513c69703b8e68f91a5da08f4007aa3d0cb9250
Ritesh <ritesh.sivanathan@gmail.com>
2025-03-19 15:05:43 -0400

    Python

---
+++ March 19th 2025/challenge.py
@@ -0,0 +1,15 @@
+'''
+Name: Ritesh Sivanathan
+Date: March 19th, 2025
+Description: Python "challenge"
+'''
+
+for i in range(1,12):
+    print(i)
+
+counter_to_hold_the_value_of_the_counter_for_the_loop = 1
+
+while counter_to_hold_the_value_of_the_counter_for_the_loop < 12:
+    print(counter_to_hold_the_value_of_the_counter_for_the_loop)
+    counter_to_hold_the_value_of_the_counter_for_the_loop += 1
+
