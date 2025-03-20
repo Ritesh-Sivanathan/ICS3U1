493e2b3f29f38b8650601d719064cfb2ae1c60a7
Ritesh <ritesh.sivanathan@gmail.com>
2025-03-20 08:24:30 -0400

    Added descriptive variable names

--- March 19th 2025/challenge.py
+++ March 19th 2025/challenge.py
@@ -1,15 +1,33 @@
 '''
 Name: Ritesh Sivanathan
 Date: March 19th, 2025
-Description: Python "challenge"
+Description: Python "challenge". Very descriptive code 😉
 '''
 
-for i in range(1,12):
-    print(i)
+def print_this_line(line_to_print):
+    print(line_to_print)
+
+def sum_of_the_items_in_this_list(the_list_to_sum_up):
+    variable_to_hold_the_sum_of_the_item_in_the_list = 0
+    for item_in_the_list_which_we_have_to_sum_up in the_list_to_sum_up:
+        variable_to_hold_the_sum_of_the_item_in_the_list += item_in_the_list_which_we_have_to_sum_up
+    return (variable_to_hold_the_sum_of_the_item_in_the_list)
+
+def tests_to_see_if_my_code_is_working_properly_or_not(output_of_the_attempts_of_the_challenge_which_are_mainly_used_to_run_the_tests_in_the_easiest_way_possible: int):
+    if output_of_the_attempts_of_the_challenge_which_are_mainly_used_to_run_the_tests_in_the_easiest_way_possible == sum_of_the_items_in_this_list([iterator_in_this_one_line_iterator_that_is_very_cool for iterator_in_this_one_line_iterator_that_is_very_cool in range(12)]):
+        print_this_line("The results of your code passed the test! The test is just seeing if the sum of the results of your code are correct. It is pretty simple but nonetheless works when doing something as simple as this. If your code didn't pass the tests, this test wouldn't be showing, so good for you! I think Python is a great language, but it sucks compared to Go. I love Go so much I wish I could do Go every day and use it for everything. Anyways, thanks for reading this, and good job on having your code pass the tests!")
+
+this_placeholder_to_hold_the_sum_of_the_first_method_to_get_the_sum_of_the_numbers_from_1_to_11_inclusive_for_the_test_only = 0
+this_second_placeholder_to_hold_the_sum_of_the_first_method_to_get_the_sum_of_the_numbers_from_1_to_11_inclusive_for_the_test_only = 0
+
+for iterator_for_this_range_loop_that_python_automatically_manages_which_is_pretty_cool_i_think_python_is_alright_but_go_is_ultimately_the_best_language in range(1,12):
+    this_placeholder_to_hold_the_sum_of_the_first_method_to_get_the_sum_of_the_numbers_from_1_to_11_inclusive_for_the_test_only += (iterator_for_this_range_loop_that_python_automatically_manages_which_is_pretty_cool_i_think_python_is_alright_but_go_is_ultimately_the_best_language)
 
 counter_to_hold_the_value_of_the_counter_for_the_loop = 1
 
 while counter_to_hold_the_value_of_the_counter_for_the_loop < 12:
-    print(counter_to_hold_the_value_of_the_counter_for_the_loop)
+    this_second_placeholder_to_hold_the_sum_of_the_first_method_to_get_the_sum_of_the_numbers_from_1_to_11_inclusive_for_the_test_only += (counter_to_hold_the_value_of_the_counter_for_the_loop)
     counter_to_hold_the_value_of_the_counter_for_the_loop += 1
 
+tests_to_see_if_my_code_is_working_properly_or_not(this_placeholder_to_hold_the_sum_of_the_first_method_to_get_the_sum_of_the_numbers_from_1_to_11_inclusive_for_the_test_only)
+tests_to_see_if_my_code_is_working_properly_or_not(this_placeholder_to_hold_the_sum_of_the_first_method_to_get_the_sum_of_the_numbers_from_1_to_11_inclusive_for_the_test_only)
