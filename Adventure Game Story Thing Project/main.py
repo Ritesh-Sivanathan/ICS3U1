df730708433c96d85346d9eed7b6bfea3ed9c9fc
Ritesh <ritesh.sivanathan@gmail.com>
2025-04-01 15:14:50 -0400

    Added stuff

--- Adventure Game Story Thing Project/main.py
+++ Adventure Game Story Thing Project/main.py
@@ -5,12 +5,21 @@ Description: (below)
 
 --- Story ---
 
-Inspired by John Grisham's `The Firm` and The `King of Torts`
+Inspired by John Grisham's `The Firm`, `The King of Torts` and a little bit of `The Broker`
 
 A lawyer finds damning evidence against a large pharmaceutical company. Before he can litigate, he is approached by a corporate fixer, 
 who offers him a bribe. His entire life is at risk no matter what choice he makes
 
+There are 5 possible outcomes
+- Accepts Bribe + Survives Attacks
+- Accepts Bribe + Doesn't Surive Attacks (dies or goes to prison)
+- Declines Bribe + Successfully takes down the pharmaceutical
+- Declines Bribe + Fails to take the pharmaceutical down and is countersued for everything
+- Declies Bribe + Runs away to another country 
+
 --- Dependencies ---
+
+Python 3.11
 
 Dependencies:
 - pandas (latest version) `pip install pandas`
@@ -40,13 +49,17 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from colorama import Fore, Style, init
 
-# -------------------------------------------------------------------------
+# Configs
 
 DEVELOPMENT_ENV = False # removes delays and other aesthetic/visual features
+LIGHTWEIGHT = True # Removes all assets / external depedencies (images and CSV)
+
+# -------------------------------------------------------------------------
+
 
 init(autoreset=True)
 
-class Format: # custom formatting class because it's more reliable
+class Format: # Custom formatting class to make inserting ANSI codes easier
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     CURSOR_UP_ONE = '\x1b[1A'
@@ -67,6 +80,66 @@ class Player:
 
         for key, value in properties.items():
             print(f"{Fore.GREEN + key}: {Format.BOLD + str(value)}")
+
+lightweight_table = ('''
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |    |        ID | Type                   | Description                                            | Date       |
+        +====+===========+========================+========================================================+============+
+        |  0 | 255431235 | Financial Transaction  | Large sum wired from offshore account to a law firm    | 2003-01-15 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |  1 | 220004129 | Email Leak             | Internal memo discussing 'silencing' a whistleblower.  | 2003-02-03 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |  2 | 268664138 | Forensic Report        | Tampered drug trial results uncovered.                 | 2003-02-17 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |  3 | 299834652 | Surveillance Footage   | Unmarked black SUV tailing key witness.                | 2003-03-01 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |  4 | 250963847 | Bank Statement         | Unusual withdrawals made by company executives.        | 2003-03-10 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |  5 | 218189729 | Confidential Testimony | Former employee reveals cover-up tactics.              | 2003-03-15 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |  6 | 259108233 | Internal Document      | Handwritten notes indicating bribery scheme.           | 2003-03-22 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |  7 | 262767883 | Phone Call Recording   | Threatening call to journalist investigating case.     | 2003-03-27 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |  8 | 223639945 | Legal Contract         | Shady settlement agreement signed under duress.        | 2003-04-02 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+        |  9 | 247701633 | Lab Report             | Evidence of manipulated test results for FDA approval. | 2003-04-10 |
+        +----+-----------+------------------------+--------------------------------------------------------+------------+
+''')
+
+lightweight_porsche = '''
+@@@(@@&&#(@@%%*@&@&(&@@&@@@@@(&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&/@(@&&#/@*&%&&@(&,& @,.@&&#,/*
+@%%@@@@*@/&@@#*#&&&&&&%%&&*%&&&#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%& (@@&,@,**%((,  ,*#*(#
+#*%%@@@@*/,..%@&&&&&(#&&*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%%%&&(%&&&@,@@&(&%%&%&&(@&&..@ #*&@&%&*,/
+%#&@%@/&@,@&@&%%&&#%@&&&,&@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@/&&&&&&&&*&*&@&&&@&/@@*&&&.%@@*&(#((,,
+&&@*@ ((@(%@@@#,@%@,,#(#(@&&&&&&&&@@@@@@&&&&&&&&&&&&%&.@&&&&&&&&%#&&&%&,&&#@&&&&&&&&&&&&&&&&&&&*@/,%
+@#@%( @,@*#.*/.&&#@@&&&&#&&&&&&&&&&&&&&&*&&&&%&#@&&&&&&&&&&&& #%&*&&&%&#&&&@(&&#&&& &.&&&&&@.& /&.,&
+*/%%&*(/##,/@#(#&&#*%.,&&&&&&&&&(&&%&&,(/,%&%&&&&&&&&&&&&&%%%&&&&/%.%&/&*@*&&&&&&&&&.@&&,&&&&&&*&,*,
+&*&#,#%%&@*&%&/%,,*,/**&*/%&&%%&/&##/%,*.,*,*.*(%&/&&&&&&.&&%&&,.% &&&@&.&&&&%#%&%%%%%%#/%*,,,@*#/*,
+##/*(/,.* ,,,***#(/,.,,,/&%&/./##(%/(/..*,,,.#%,/*&(/*(,/&&( ,%/%& (##%#/(%& ,*,,&&&&&&&&&&&*.*,,.//
+//(%*//,(*/,%*,,.,*./.((/*,*,.., % */*..,,,,,. ,,****%#((((((((((/%/###&%,(&.&/&(%%.#&.,.(/(.. (,//*
+*(#*,/**/..,,, ,..,..,.*,,. ...* .,./ .,(.*,*, %%%#%#####(##(((((#(*,##%.*,#/*.*((/%.(**., *./.,,.,,
+,/.,,,(((,*,, /(*/    *,, ,....,**,,,**..,,.%%%#####((#(((((((%&(*(,(#,   %%%%,*****,,..(*,*,,.,...*
+ ,(*...,.,*/,*.,..,*.,.,  *,, %#(,,/,,,.*/,*#%%%%#######(#(/***/#%*,  /..... ...,.**,/*.,.,   ,.,, .
+,,*.,*/,*,.,, ,,.,*/,/,,(*.&@((*%#%###(%%##########/%#(/*/#/////.  ,....   . ., *(,*/*,*.. *.**.***.
+.   .*.. ,,**(/*/**,.*,*#%@#%%/##((#########((###/%#@#/%&%/****.* . ,,,,..*,..  /,(*/*///*(/*/,,**##
+&&%%&%&&&&&%&&%%%%%%%%&&#(%#%##########(##((#(((((@&& (///**...    , ,,,,,*...  ,/(((((#*##(##.#####
+####@#####(##((((#(###%%################((((((((####(((//**.., .   . ,,*,..,   .**////(/(/@@((((/(((
+####(##(@#((#(###(#(#*,,*##/%(##.#%&@&&%%#@%/.,/#((   ,.....,,     ,....  .,,/*///////(//@&#//((((((
+(##((###(#(((########,    ..,,.. ,,,,,,         *   . ........   . . ..,.,/(/////((//((@&(&/((((/(#(
+###((##(#####((((#%(#%,. ,             ,.*         .....,.,.,.      */*,*//*/(///((/((&%*#(((//#/(#(
+###%#((((/(#@&###((#(/,,,...** **%%,*//*&#%*.%%%%%##,,,......       /*/////((///((((/@@@%*//////(/(/
+##(/@@((((#/#%#%(##(##/**..*/(*///**/,.,,..,,,,,,          .,,,**,,(((((((/(((/((//&@.@#//#(///(/*/(
+(((#(#(##((#/##(((/%((/(***,*.,... . .  .    ... . ,.,,,.*/*/,,*#(/((%%((##(((/(((%@@##/((///(((*(/(
+%#/(*(##/(//(//((%/(#/*((#/#/((//*((///#/**((*(/(//#(/(*#/,((/(((/((%(//#((#*//((&&@//*(/(#//(/*/(*(
+((//(#((##/%(%*##/((%#(((#%%#//((/%//##((#((%((/%#%#(,*#(%(%%/#(((%%(#(/#/(///#/@&%#(//(///#(/////(/
+((#/%*#(%//%%(%%#&#%(&/*%##((###(///%/(##((/#&***,*@&(**(##(#((#(**(#%((#/#((/#&&/*%%(#(/(**(/(/(,/(
+(,(,(%#/%%%%##/(#((###,*(*/(%(&%(%/##((#,//##%(%((////##//####((/(#/(/(#/#*(/#@&@&*,*,//(*//(*//(*((
+*/*(/,/%#(((&##(%%###(/((/(*(%%(*&*,(#/(##(###%#**#(%%#&##//#((%*##%/#(/%(//@&&@@*,**(/(/#%/////(/(/
+(&%##*,(#//,(#***(#%((###(/#/**%*%/##%#%%(%#/#***#@##(&((/%##/(/#(((&(((/##&@&&%/(,*#/((////,,/#/*(/
+(##(/((**(*//**%%(/%###%(/*(//%/(%#/(#%#&#(/*#**#@(&###(#*(((#(/#(//**//#,@@&*(,,/(//(*(/*#(////*///
+
+'''
 
 class utils:
 
@@ -124,7 +197,7 @@ newPlayer = Player(5, 20)
 
 def intro():
 
-    utils.type_text("\nMoultrie Courthouse, Washington D.C - 3:03 PM\n", color=Fore.RED)
+    utils.type_text("\nMoultrie Courthouse, Washington D.C - May 6th, 2003 | 3:03 PM\n", color=Fore.RED)
     utils.type_text("\nThe courtroom hums with quiet chatter as you sit behind your desk, flipping through the case files.")
     utils.type_text("\nThe name of the defendant, a former researcher at Vindex Pharmaceuticals, glares at you from the page.")
     utils.type_text("\nThe evidence is damning, yet something doesn’t sit right. Witness testimonies contradict, documents appear altered, and an anonymous source has hinted at something much bigger.")
@@ -132,25 +205,26 @@ def intro():
     utils.type_text("\nYou look over the activity summary of Vindex that you drafted yesterday.\n")
     
     for i in range(3, 0, -1):
-        utils.type_text(f"Data showing in {i}", color=Fore.MAGENTA)
+        utils.type_text(f"Data showing in {i}\n", color=Fore.MAGENTA)
         time.sleep(1)
 
     suspiciousEvidence = pd.read_csv('./assets/suspicious_evidence.csv')
-        
+
     print('\n' + '-' * 20 + '\n')
-        
-    table_str = tabulate(suspiciousEvidence, headers=suspiciousEvidence.columns, tablefmt="grid")
-    print(table_str) 
+     
+    if LIGHTWEIGHT: 
+        print(lightweight_table)
+    else:
+        table_str = tabulate(suspiciousEvidence, headers=suspiciousEvidence.columns, tablefmt="grid")
+        print(table_str)
 
     print('\n' + '-' * 20)
     
     utils.type_text("Read the data!\n")
-    
-    if not DEVELOPMENT_ENV:
-        for i in range(15,1,-1):
-            print(i)
-            time.sleep(1)
-            utils.delete_last_line()
+    for i in range(16, 0,-1):
+        print(i)
+        time.sleep(1)
+        utils.delete_last_line()
 
     utils.type_text("\n\"Mr.Carter!\". You look up to everyone in the courtroom staring at you. You request for a postponement of the hearing to review evidence, and the judge approves.\n")
     utils.type_text("Later that night, a shadowy figure steps into your office. Dressed in a sharp suit, he slides a briefcase across your desk and makes an offer to drop the case and forget about all of this.\n", color=Fore.RED)
@@ -165,7 +239,7 @@ def intro():
         utils.type_text("Invalid choice. Try again.", color=Fore.RED)
         intro()
 
-def accept_bribe():
+def accept_bribe(): # Storyline 1
 
     utils.type_text("\nYour fingers tighten around the briefcase handle as you weigh the consequences.\n")
     utils.type_text("It’s more money than you’ve ever seen in your life. A way out. A fresh start.\n")
@@ -212,21 +286,27 @@ def accept_bribe():
             newPlayer.userStats()
 
             if not shown_success_msg:
-                utils.type_text("You notice people getting more distant. More suspicious of you. You brush it off as paranoia, but deep down you know something big is on the horizon.")
+                utils.type_text("\nYou notice people getting more distant. More suspicious of you. You brush it off as paranoia, but deep down you know something big is on the horizon.\n")
             
             if (not shown_success_msg):
                 
                 utils.type_text("\nYou're still making millions, though.", color=Fore.GREEN)
-                utils.type_text("\nYou've purchased a few new toys, a downtown condo and have your own law firm.")
+                utils.type_text("\nYou've purchased a few new toys, a downtown condo and have your own law firm.\n")
                 
-                if not DEVELOPMENT_ENV:
+                time.sleep(2)
+
+                if not DEVELOPMENT_ENV and not LIGHTWEIGHT:
                     utils.display_image('./assets/porsche.jpg', title="New Car")
                     utils.display_image('./assets/condo.jpg', title="Luxury Condo - Downtown D.C")
                     utils.display_image('./assets/watches.jpg', title="Watch collection")
-                
-                utils.type_text("Life is great.")
+                    utils.type_text("Life is great.")
+                elif LIGHTWEIGHT:
+                    utils.type_text("\nBrand New Sports Car\n", color=Fore.CYAN)
+                    print(lightweight_porsche)
 
-                utils.type_text("\nYou reflect on how your life went from barely surviving in a run down apartment in the worst area of D.C to becoming the Wolf of K Street ")
+                time.sleep(2)
+                
+                utils.type_text("\nYou reflect on how your life went from barely surviving in a run down apartment in the worst area of D.C to becoming the Wolf of K Street\n")
                 
                 shown_success_msg = True
 
@@ -246,7 +326,8 @@ def accept_bribe():
     
     FBI_Finds_Out() # runs after all the events (unless game ends)
 
-def reject_bribe():
+def reject_bribe(): # Storyline 2
+
     utils.type_text("Your jaw tightens. You push the briefcase back across the desk.", color=Fore.RED)
     utils.type_text("\nThe fixer smirks. ‘You’ll regret this,’ he says before disappearing into the night.")
     utils.type_text("\nDays later, your world starts unraveling.")
@@ -324,10 +405,10 @@ def fight_or_flight():
         flee_or_fight = input("Do you flee the country or fight the law on this? (flee/fight)")
         if flee_or_fight == "flee":
             utils.type_text("You catch the next flight out of the U.S. You land in Germany and fly from there to Italy, where you have a citizenship.")
-            utils.type_text("You live your life quietly with your millions, but still have that feeling like someone is watching you...", color=Fore.RED)
+            utils.type_text("You live your life quietly in Bologna with your millions, but still have that feeling like someone is watching you...", color=Fore.RED)
         elif flee_or_fight == "fight":
             utils.type_text("You choose to stay in D.C and fight the law.")
-            utils.type_text("You're suddenly awaken in the middle of the night. You check your clock, and see that it's 3:03AM.")
+            utils.type_text("You're suddenly awaken in the middle of the night. You check your clock, and see that it's 1:52AM.")
             utils.type_text("\"This is the FBI. We have a warrant for your arrest. \"", color=Fore.RED)
             game_over()
 
@@ -360,11 +441,11 @@ def FBI_Finds_Out():
 
 def Interrogation():
     
-    utils.type_text("\nIn the quiet of an empty conference room deep in the FBI Headquarters, a team of field agents, lead by the Executive Assistant Director of the head office meet you.")
-    utils.type_text("After a few moments of silence, the AD breaks the silence. \"We know what you've done.\"")
-    utils.type_text("Your stay calm because you know you've covered you tracks well.")
-    utils.type_text("\"If you confess to all your crimes now, we can offer you protective custody at the best white collar prison in the country as well as a reduced sentence.\"")
-    offer = input("Do you take the bait and accept the deal or do you decline? (accept/decline): ")
+    utils.type_text("\n\nIn the quiet of an empty conference room deep in the FBI Headquarters, a team of field agents, lead by the Executive Assistant Director of the head office meet you.")
+    utils.type_text("\nAfter a few moments of silence, the AD breaks the silence. \"We know what you've done.\"")
+    utils.type_text("\nYou stay calm because you know you've covered your tracks well.")
+    utils.type_text("\n\"If you confess to all your crimes now, we can offer you protective custody at the best white collar prison in the country as well as a reduced sentence.\"")
+    offer = input(Fore.RED + "\nDo you take the bait and accept the deal or do you decline? (accept/decline): ")
 
     if offer.strip().lower() == "accept":
     
@@ -374,13 +455,26 @@ def Interrogation():
         game_over()
 
     elif offer.strip() == "decline":
-        pass
-    
+        utils.type_text("\"Alright, Mr.Carter, just know that we're coming after you whether you choose to confess or not. You'd better watch your back. Get him out of here.\"")
+        utils.type_text("The field agents drag you out of the building and you're left to catch a cab home.")
+        call_lawyer = (input("Once you get home, do you call your lawyer, or ignore what happened? (lawyer/ignore): "))
+
+        if call_lawyer.strip().lower() == "lawyer":
+            
+            utils.type_text("You immediately call your lawyer, who is enraged by the fact that the FBI pulled you into their building without him present.")
+            utils.type_text("He schedules a meeting with a prominent journalist for tomorrow to publicize this information.")
+            publicize_or_not = input("Do you want to go public? (yes/no): ")
+            
+            if publicize_or_not.strip().lower() == "yes":
+                utils.type_text("Multiple groups whose sole purpose is to kill you catch wind that you're headed to a \"secret\" meeting with a journalist. They don't know who's being targeted so they all want you dead ")
+                utils.type_text("As you wait at a traffic light, just a few blocks from the journal's office, you hear a car's tires screech behind you")
+                utils.type_text("Suddently, everything goes black. ")
+
 def game_over():
 
     newPlayer.userStats()
 
-    utils.type_text("\nGAME OVER.", color=Fore.RED, bold=True)
+    utils.type_text("\nGAME OVER.\n", color=Fore.RED, bold=True)
     
     retry = input(Fore.GREEN + "Would you like to try again? (yes/no):").strip().lower()
     
